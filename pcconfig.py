65889e8bf8d539683e12d5343d74451167ea54ca
Beppvis <67070783+beppvis@users.noreply.github.com>
2023-02-18 16:56:48 +0530

    Delete pcconfig.py

--- pcconfig.py
+++
@@ -1,8 +0,0 @@
-import pynecone as pc
-
-config = pc.Config(
-    app_name="Chemistry hmm",
-    api_url="https://beppvis.github.io/chemistry-praticlas",
-    db_url="sqlite:///pynecone.db",
-    env=pc.Env.DEV,
-)
